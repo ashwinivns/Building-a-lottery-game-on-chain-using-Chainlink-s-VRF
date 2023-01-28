9c3c76d15a92b0b56fb3beee469356158bd70b05
ashwini <ashwinikantpandey295>
2023-01-28 19:30:12 +0530

    the Graph

--- hardhat-tutorial/scripts/deploy.js
+++ hardhat-tutorial/scripts/deploy.js
@@ -1,6 +1,11 @@
 const { ethers } = require("hardhat");
 require("dotenv").config({ path: ".env" });
-const { FEE, VRF_COORDINATOR, LINK_TOKEN, KEY_HASH } = require("../constants");
+const {
+  FEE,
+  VRF_COORDINATOR,
+  LINK_TOKEN,
+  KEY_HASH,
+} = require("../constants/index");
 
 async function main() {
   /*
